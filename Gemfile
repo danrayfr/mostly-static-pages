b660d7511aaa20c880fe3be580e17ffbca883c88
danrayfr <danrayrollan98@gmail.com>
2023-02-06 14:28:10 +0800

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -75,3 +75,5 @@ group :test do
   gem "guard", "~> 2.18"
   gem "guard-minitest", "~> 2.4"  
 end
+
+gem "bootstrap-sass", "~> 3.4"
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # enable debug mode
+  config.assets.debug = true
 end
