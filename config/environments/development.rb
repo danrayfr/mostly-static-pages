86367e314f6b74689771e311ade65e0405422718
danrayfr <danrayrollan98@gmail.com>
2023-02-13 14:21:53 +0800

    Add password reset

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,9 +39,21 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :test
+
   host = 'localhost:3000' # Local server
 
   config.action_mailer.default_url_options = { host: host, protocol: 'http' } # Use https if deploy on cloud
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => "danrayrollan98@gmail.com",
+    :password             => Rails.application.credentials.dig(:gmail_auth_key),
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
 
   config.action_mailer.perform_caching = false
 
